a03cf3347d0fc09664c6fb27b9716a8d48044674
Proskurin A <forge2010@rambler.ru>
2023-06-26 14:44:31 +0300

    2,3 tasks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 
-s = ' asdad fsafsa fsafasАдрес 54\\45. Номер 40\\59 fasfasfsaf fas 6622\\66333 a'
+# 1 задание
+
+s = 'Адрес 54\\45. Номер 40\\59  6622\\66333'
 result = []
 
 
@@ -15,3 +17,35 @@ def good_number(string):
 
 print(good_number(s))
 
+# 2 задание
+
+def add_atms(n, k, distances):
+
+    result= list()
+
+    for i in range(k) :
+        max_num = max(distances)
+        max_index = distances.index(max_num)
+        last_pos = max(distances)
+
+        distances[max_index] =  last_pos/2
+        distances.insert(max_index, last_pos / 2)
+
+    return distances
+
+n = 5
+k = 3
+distances = [100, 180, 50, 60, 150]
+
+new_distances = add_atms(n, k, distances)
+
+print(new_distances)
+
+# 3 задание
+
+def max_concatenated_number(lst):
+    sorted_lst = sorted(lst, reverse=True)
+    concatenated_str = ''.join(sorted_lst)
+    return int(concatenated_str)
+lst = ['50', '2', '1', '9']
+print(max_concatenated_number(lst))
